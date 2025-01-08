6a508db5fca4cc10bbeb0162142a3c969bd9ee09
yogeshgheu <yogeshgheu@gmail.com>
2025-01-08 14:45:26 +0530

    add user Model using mongoose

---
+++ models/user.model.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import "dotenv/config";
+
+const userSchema = mongoose.Schema(
+	{
+		firstName: {
+			type: String,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			required: true,
+		},
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		phone: {
+			type: String,
+			required: true,
+		},
+		profilePicture: {
+			type: String,
+			default: process.env.DEFAULT_PROFILE_PICTURE,
+		},
+		isActive: {
+			type: Boolean,
+			default: true,
+		},
+		isVerified: {
+			type: Boolean,
+			default: false,
+		},
+		role: {
+			type: String,
+			enum: ["user", "admin"],
+			default: "user",
+		},
+		refreshToken: {
+			type: String,
+			unique: true,
+			required: false,
+		},
+	},
+	{ timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+	if (!this.isModified("password")) return next();
+	const salt = await bcrypt.genSalt(10);
+	const encryptedPassword = await bcrypt.hash(this.password, salt);
+	this.password = encryptedPassword;
+	next();
+});
+
+userSchema.methods.matchPassword = async function (password) {
+	return await bcrypt.compare(password, this.password); // returns true or false
+};
+
+export const User = mongoose.model("User", userSchema);
